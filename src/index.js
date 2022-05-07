3359477877668c125fba6c2ca26b06f686431129
Ben Schoelkopf <thebenschoel@gmail.com>
2022-05-07 11:46:06 -0400

    how abhout his

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <h1>ben was here</h1>
-        <test></test>
     </React.StrictMode>
 );
