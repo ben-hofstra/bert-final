459e41306af023bce100c3b7ba5de2adc7a59d73
ben <ben@mac-mini.dhcp.hofstra.edu>
2022-05-07 11:45:04 -0400

    mighta broke someting

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <h1>ben was here</h1>
+        <test></test>
     </React.StrictMode>
 );
